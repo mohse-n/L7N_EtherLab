2a4cc80d822637d52c6469d7e0d849254e907aee
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-09-25 16:01:06 +0330

    Added comments for ecrt_master_slave_config

--- simple_test.c
+++ simple_test.c
@@ -1,5 +1,13 @@
 #include "ecrt.h"
-sc = ecrt_master_slave_config( ... )
+
+/* See line 665 of ethercat/include/ecrt.h */
+sc = ecrt_master_slave_config(
+        ec_master_t *master, /**< EtherCAT master */
+        uint16_t alias, /**< Slave alias. */
+        uint16_t position, /**< Slave position. */
+        uint32_t vendor_id, /**< Expected vendor ID. */
+        uint32_t product_code /**< Expected product code. */
+        );
 /* See line 524 of ethercat/master/slave_config.c */
 ecrt_slave_config_sync_manager(sc, 2, EC_DIR_INPUT, ? )
-ecrt_slave_config_sync_manager(sc, 2, EC_DIR_INPUT, ? )
+ecrt_slave_config_sync_manager(sc, 3, EC_DIR_INPUT, ? )
