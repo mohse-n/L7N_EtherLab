2d8af9ad528ea86614a7404eb43419c0d43049ae
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-09-27 10:22:52 +0330

    Minimal example: Reserve a master

---
+++ simple_test.c
@@ -0,0 +1,14 @@
+#include "ecrt.h"
+
+
+int main(int argc, char **argv)
+{
+
+	ec_master_t* master;
+	/* Reserve the first master (0) (/etc/init.d/ethercat start) for this program */
+	master = ecrt_request_master(0);
+	if (!master)
+		return -1;
+	/* This thread sleeps untill a signal is delivered that either terminates it or causes the invocation of a signal-catching function */
+	pause();
+}
