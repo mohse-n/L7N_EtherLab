6792b92317dc106a2fa8b7fd2ad2d555907dfc44
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-09-27 11:20:28 +0330

    Write controlword

--- simple_test.c
+++ simple_test.c
@@ -11,5 +11,11 @@ int main(int argc, char **argv)
 	master = ecrt_request_master(0);
 	if (!master)
 		printf("Requesting master failed\n");
-
+	
+	uint8_t controlWord = 0xF;
+	uint8_t retVal;
+	
+	retVal = ecrt_master_sdo_download(master, 0, 0x6040, 0x00, &controlWord, sizeof(controlWord), NULL)
+	if (!retVal)
+		printf("OD write successful\n");
 }
