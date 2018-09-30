aec4cf5b53652473e2d99c1d39a97d7a72973777
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-09-30 12:50:10 +0330

    cstruct structures

--- simple_test.c
+++ simple_test.c
@@ -36,7 +36,40 @@ int main(int argc, char **argv)
 	if (!drive0)
 	{
 		printf("Failed to get slave configuration\n");
+		return -1;
 	}
+	
+	/* Structures obtained from $ethercat cstruct -p 0 */
+	/***************************************************/
+	
+	ec_pdo_entry_info_t slave_0_pdo_entries[] = 
+	{
+	{0x6040, 0x00, 16}, /* Controlword */
+	{0x607a, 0x00, 32}, /* Target Position */
+	{0x6041, 0x00, 16}, /* Statusword */
+	{0x6064, 0x00, 32}, /* Position Actual Value */
+	};
+	
+	ec_pdo_info_t slave_0_pdos[] =
+	{
+	{0x1601, 2, slave_0_pdo_entries + 0}, /* 2nd Receive PDO Mapping */
+	{0x1a01, 2, slave_0_pdo_entries + 2}, /* 2nd Transmit PDO Mapping */
+	};
+	
+	ec_sync_info_t slave_0_syncs[] =
+	{
+	{0, EC_DIR_OUTPUT, 0, NULL            , EC_WD_DISABLE},
+	{1, EC_DIR_INPUT , 0, NULL            , EC_WD_DISABLE},
+	{2, EC_DIR_OUTPUT, 1, slave_0_pdos + 0, EC_WD_DISABLE},
+	{3, EC_DIR_INPUT , 1, slave_0_pdos + 1, EC_WD_DISABLE},
+	{0xFF}
+	};
+	
+	/***************************************************/
+	
+	
+	
+	
 	
 	return 0;
 }
