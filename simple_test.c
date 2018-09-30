e9a8522d9dd48daba6425fc12f595f713a1dec0a
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-09-30 10:42:33 +0330

    Slave configuration

--- simple_test.c
+++ simple_test.c
@@ -21,7 +21,22 @@ int main(int argc, char **argv)
 	if (!master)
 		printf("Requesting master failed\n");
 	
-	ODwrite(master, 0, 0x6040, 0x00, 0xF);
+	//ODwrite(master, 0, 0x6040, 0x00, 0xF);
+	
+	uint16_t alias0 = 0;
+	uint16_t position0 = 0;
+	uint32_t vendor_id0 = 0x00007595;
+	uint32_t product_code0 = 0x00000000;
+	
+	/* Creates and returns a slave configuration object, ec_slave_config_t*, for the given alias and position */
+	/* Returns NULL in case of error and pointer to the configuration struct otherwise */
+	ec_slave_config_t* drive0 = ecrt_master_slave_config(master, alias0, position0, vendor_id0, produc_code0);
+	
+	/* If the drive0 = NULL */
+	if (!drive0)
+	{
+		printf("Failed to get slave configuration\n");
+	}
 	
 	return 0;
 }
