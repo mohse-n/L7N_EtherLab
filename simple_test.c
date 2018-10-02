cb57c0f876b895abe11ed3f2accf7021750fe2e9
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-10-02 12:11:11 +0330

    Added the second slave to code

--- simple_test.c
+++ simple_test.c
@@ -32,17 +32,19 @@ int main(int argc, char **argv)
 	ODwrite(master, 0, 0x6040, 0x00, 0xF);
 	ODwrite(master, 0, 0x6060, 0x00, 0x8);
 	
-	uint16_t alias0 = 0;
+	uint16_t alias = 0;
 	uint16_t position0 = 0;
-	uint32_t vendor_id0 = 0x00007595;
-	uint32_t product_code0 = 0x00000000;
+	uint16_t position1 = 1;
+	uint32_t vendor_id = 0x00007595;
+	uint32_t product_code = 0x00000000;
 	
 	/* Creates and returns a slave configuration object, ec_slave_config_t*, for the given alias and position. */
-	/* Returns NULL in case of error and pointer to the configuration struct otherwise */
-	ec_slave_config_t* drive0 = ecrt_master_slave_config(master, alias0, position0, vendor_id0, product_code0);
+	/* Returns NULL (0) in case of error and pointer to the configuration struct otherwise */
+	ec_slave_config_t* drive0 = ecrt_master_slave_config(master, alias, position0, vendor_id, product_code);
+	ec_slave_config_t* drive1 = ecrt_master_slave_config(master, alias, position1, vendor_id, product_code);
 	
-	/* If the drive0 = NULL */
-	if (!drive0)
+	/* If the drive0 = NULL or drive1 = NULL */
+	if (!drive0 || !drive1)
 	{
 		printf("Failed to get slave configuration\n");
 		return -1;
@@ -50,7 +52,7 @@ int main(int argc, char **argv)
 	
 	/* Structures obtained from $ethercat cstruct -p 0 */
 	/***************************************************/
-	
+	/* Slave 0's structures */
 	ec_pdo_entry_info_t slave_0_pdo_entries[] = 
 	{
 	{0x6040, 0x00, 16}, /* Controlword */
@@ -74,23 +76,57 @@ int main(int argc, char **argv)
 	{0xFF}
 	};
 	
+	/* Slave 1's structures */
+	ec_pdo_entry_info_t slave_1_pdo_entries[] = 
+	{
+	{0x6040, 0x00, 16}, /* Controlword */
+	{0x607a, 0x00, 32}, /* Target Position */
+	{0x6041, 0x00, 16}, /* Statusword */
+	{0x6064, 0x00, 32}, /* Position Actual Value */
+	};
+	
+	ec_pdo_info_t slave_1_pdos[] =
+	{
+	{0x1601, 2, slave_1_pdo_entries + 0}, /* 2nd Receive PDO Mapping */
+	{0x1a01, 2, slave_1_pdo_entries + 2}, /* 2nd Transmit PDO Mapping */
+	};
+	
+	ec_sync_info_t slave_1_syncs[] =
+	{
+	{0, EC_DIR_OUTPUT, 0, NULL            , EC_WD_DISABLE},
+	{1, EC_DIR_INPUT , 0, NULL            , EC_WD_DISABLE},
+	{2, EC_DIR_OUTPUT, 1, slave_1_pdos + 0, EC_WD_DISABLE},
+	{3, EC_DIR_INPUT , 1, slave_1_pdos + 1, EC_WD_DISABLE},
+	{0xFF}
+	};
+	
 	/***************************************************/
 	
 	if (ecrt_slave_config_pdos(drive0, EC_END, slave_0_syncs))
 	{
-		printf("Failed to configure PDOs\n");
+		printf("Failed to configure slave 0 PDOs\n");
 		return -1;
 	}
 	
+	if (ecrt_slave_config_pdos(drive1, EC_END, slave_1_syncs))
+	{
+		printf("Failed to configure slave 1 PDOs\n");
+		return -1;
+	}
 
 	unsigned int offset_controlWord, offset_targetPos, offset_statusWord, offset_actPos;
 	
 	ec_pdo_entry_reg_t domain1_regs[] =
 	{
-	{0, 0, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord},
-	{0, 0, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos  },
-	{0, 0, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord },
-	{0, 0, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos     },
+	{0, 0, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord0},
+	{0, 0, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos0  },
+	{0, 0, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord0 },
+	{0, 0, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos0     },
+	
+	{0, 1, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord1},
+	{0, 1, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos1  },
+	{0, 1, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord1 },
+	{0, 1, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos1     },
 	{}
 	};
 	
@@ -132,8 +168,8 @@ int main(int argc, char **argv)
 	
 	
 	int i = 0;
-	int32_t actPos;
-	int32_t targetPos;
+	int32_t actPos0, actPos1;
+	int32_t targetPos0, targetPos1;
 	
 	
 	
@@ -151,14 +187,14 @@ int main(int argc, char **argv)
 		/********************************************************************************/
 		
 		/* Read PDOs from the datagram */
-		actPos = EC_READ_S32(domain1_pd + offset_actPos);
+		actPos0 = EC_READ_S32(domain1_pd + offset_actPos0);
 		
 		/* Process the received data */
-		targetPos = actPos + ENCODER_RES;
+		targetPos0 = actPos0 + ENCODER_RES;
 		
 		/* Write PDOs to the datagram */
-		EC_WRITE_U8  (domain1_pd + offset_controlWord, 0xF );
-		EC_WRITE_S32 (domain1_pd + offset_targetPos  , targetPos);
+		EC_WRITE_U8  (domain1_pd + offset_controlWord0, 0xF );
+		EC_WRITE_S32 (domain1_pd + offset_targetPos0  , targetPos0);
 		
 		/********************************************************************************/
 		/* Queues all domain datagrams in the master's datagram queue. 
