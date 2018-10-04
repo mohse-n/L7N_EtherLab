cc4f3e165568dd2eba6dbaebf665ccce99d90970
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-10-04 22:42:24 +0330

    offsets and pdo entries

--- RTAI/rtai_sample.c
+++ RTAI/rtai_sample.c
@@ -10,5 +10,20 @@ static uint8_t* domain1_pd;
 static ec_slave_config_t* drive0 = NULL;
 static ec_slave_config_t* drive1 = NULL;
 
+static unsigned int offset_controlWord0, offset_targetPos0, offset_statusWord0, offset_actPos0;
+static unsigned int offset_controlWord1, offset_targetPos1, offset_statusWord1, offset_actPos1;
 
+const static ec_pdo_entry_reg_t domain1_regs[] =
+	{
+	{0, 0, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord0},
+	{0, 0, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos0  },
+	{0, 0, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord0 },
+	{0, 0, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos0     },
+	
+	{0, 1, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord1},
+	{0, 1, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos1  },
+	{0, 1, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord1 },
+	{0, 1, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos1     },
+	{}
+	};
 
