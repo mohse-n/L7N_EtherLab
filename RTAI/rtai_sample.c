e4254eb44a0fa613ed5d5cef8d91ab9395cdec42
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-10-04 22:40:35 +0330

    Header and general variables

--- RTAI/rtai_sample.c
+++ RTAI/rtai_sample.c
@@ -1,1 +1,14 @@
+#include "../../include/ecrt.h"
+
+
+
+
+static ec_master_t* master;
+static ec_domain_t* domain1 = NULL;
+static uint8_t* domain1_pd;
+
+static ec_slave_config_t* drive0 = NULL;
+static ec_slave_config_t* drive1 = NULL;
+
+
 
