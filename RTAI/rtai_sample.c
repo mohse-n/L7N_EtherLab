74abf7e8db8af81d17bb7770c84f685561a52f99
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-10-05 12:17:31 +0330

    Config PDOs

--- RTAI/rtai_sample.c
+++ RTAI/rtai_sample.c
@@ -105,7 +105,19 @@ int __init init_mod(void)
 	/* Creates and returns a slave configuration object, ec_slave_config_t*, for the given alias and position. */
 	/* Returns NULL (0) in case of error and pointer to the configuration struct otherwise */
 	ec_slave_config_t* drive0 = ecrt_master_slave_config(master, alias, position0, vendor_id, product_code);
-	ec_slave_config_t* drive1 = ecrt_master_slave_config(master, alias, position1, vendor_id, product_code);	
+	ec_slave_config_t* drive1 = ecrt_master_slave_config(master, alias, position1, vendor_id, product_code);
+
+	if (ecrt_slave_config_pdos(drive0, EC_END, slave_0_syncs))
+	{
+		printf("Failed to configure slave 0 PDOs\n");
+		return -1;
+	}
+	
+	if (ecrt_slave_config_pdos(drive1, EC_END, slave_1_syncs))
+	{
+		printf("Failed to configure slave 1 PDOs\n");
+		return -1;
+	}	
 	
 	
 }
