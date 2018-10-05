2fbf19021f8ce990cbf6c42758a78bae89c4c460
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-10-05 12:19:54 +0330

    Changed order of structure definitions

--- RTAI/rtai_sample.c
+++ RTAI/rtai_sample.c
@@ -12,23 +12,6 @@ static uint8_t* domain1_pd;
 static ec_slave_config_t* drive0 = NULL;
 static ec_slave_config_t* drive1 = NULL;
 
-static unsigned int offset_controlWord0, offset_targetPos0, offset_statusWord0, offset_actPos0;
-static unsigned int offset_controlWord1, offset_targetPos1, offset_statusWord1, offset_actPos1;
-
-const static ec_pdo_entry_reg_t domain1_regs[] =
-{
-	{0, 0, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord0},
-	{0, 0, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos0  },
-	{0, 0, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord0 },
-	{0, 0, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos0     },
-	
-	{0, 1, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord1},
-	{0, 1, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos1  },
-	{0, 1, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord1 },
-	{0, 1, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos1     },
-	{}
-};
-	
 /* Structures obtained from $ethercat cstruct -p 0 */
 /***************************************************/
 /* Slave 0's structures */
@@ -79,7 +62,25 @@ static ec_sync_info_t slave_1_syncs[] =
 	{0xFF}
 };
 	
-	/***************************************************/
+/***************************************************/
+
+static unsigned int offset_controlWord0, offset_targetPos0, offset_statusWord0, offset_actPos0;
+static unsigned int offset_controlWord1, offset_targetPos1, offset_statusWord1, offset_actPos1;
+
+const static ec_pdo_entry_reg_t domain1_regs[] =
+{
+	{0, 0, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord0},
+	{0, 0, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos0  },
+	{0, 0, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord0 },
+	{0, 0, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos0     },
+	
+	{0, 1, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord1},
+	{0, 1, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos1  },
+	{0, 1, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord1 },
+	{0, 1, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos1     },
+	{}
+};
+/***************************************************/	
 
 /* Why define data? */
 void run(long data)
