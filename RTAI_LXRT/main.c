977b6f99e03fe1f6c93856271b6e492e9ee26c9a
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-10-14 13:41:16 +0330

    Copy-pasted from user example

--- RTAI_LXRT/main.c
+++ RTAI_LXRT/main.c
@@ -1,1 +1,169 @@
+int main(int argc, char **argv)
+{
+	/* Lock the program into RAM and prevent swapping */
+	if (mlockall(MCL_CURRENT | MCL_FUTURE) == -1)
+	{
+		printf("mlockall failed");
+		return -1;
+	}
+	
+	signal(SIGINT, signal_handler);
+	
+	/* Reserve the first master (0) (/etc/init.d/ethercat start) for this program */
+	master = ecrt_request_master(0);
+	if (!master)
+		printf("Requesting master failed\n");
+	
+	initDrive(master, 0);
+	initDrive(master, 1);
+	
+	uint16_t alias = 0;
+	uint16_t position0 = 0;
+	uint16_t position1 = 1;
+	uint32_t vendor_id = 0x00007595;
+	uint32_t product_code = 0x00000000;
+	
+	/* Creates and returns a slave configuration object, ec_slave_config_t*, for the given alias and position. */
+	/* Returns NULL (0) in case of error and pointer to the configuration struct otherwise */
+	ec_slave_config_t* drive0 = ecrt_master_slave_config(master, alias, position0, vendor_id, product_code);
+	ec_slave_config_t* drive1 = ecrt_master_slave_config(master, alias, position1, vendor_id, product_code);
+	
+	
+	/* If the drive0 = NULL or drive1 = NULL */
+	if (!drive0 || !drive1)
+	{
+		printf("Failed to get slave configuration\n");
+		return -1;
+	}
+	
+	/* Structures obtained from $ethercat cstruct -p 0 */
+	/***************************************************/
+	/* Slave 0's structures */
+	ec_pdo_entry_info_t slave_0_pdo_entries[] = 
+	{
+	{0x6040, 0x00, 16}, /* Controlword */
+	{0x607a, 0x00, 32}, /* Target Position */
+	{0x6041, 0x00, 16}, /* Statusword */
+	{0x6064, 0x00, 32}, /* Position Actual Value */
+	};
+	
+	ec_pdo_info_t slave_0_pdos[] =
+	{
+	{0x1601, 2, slave_0_pdo_entries + 0}, /* 2nd Receive PDO Mapping */
+	{0x1a01, 2, slave_0_pdo_entries + 2}, /* 2nd Transmit PDO Mapping */
+	};
+	
+	ec_sync_info_t slave_0_syncs[] =
+	{
+	{0, EC_DIR_OUTPUT, 0, NULL            , EC_WD_DISABLE},
+	{1, EC_DIR_INPUT , 0, NULL            , EC_WD_DISABLE},
+	{2, EC_DIR_OUTPUT, 1, slave_0_pdos + 0, EC_WD_DISABLE},
+	{3, EC_DIR_INPUT , 1, slave_0_pdos + 1, EC_WD_DISABLE},
+	{0xFF}
+	};
+	
+	/* Slave 1's structures */
+	ec_pdo_entry_info_t slave_1_pdo_entries[] = 
+	{
+	{0x6040, 0x00, 16}, /* Controlword */
+	{0x607a, 0x00, 32}, /* Target Position */
+	{0x6041, 0x00, 16}, /* Statusword */
+	{0x6064, 0x00, 32}, /* Position Actual Value */
+	};
+	
+	ec_pdo_info_t slave_1_pdos[] =
+	{
+	{0x1601, 2, slave_1_pdo_entries + 0}, /* 2nd Receive PDO Mapping */
+	{0x1a01, 2, slave_1_pdo_entries + 2}, /* 2nd Transmit PDO Mapping */
+	};
+	
+	ec_sync_info_t slave_1_syncs[] =
+	{
+	{0, EC_DIR_OUTPUT, 0, NULL            , EC_WD_DISABLE},
+	{1, EC_DIR_INPUT , 0, NULL            , EC_WD_DISABLE},
+	{2, EC_DIR_OUTPUT, 1, slave_1_pdos + 0, EC_WD_DISABLE},
+	{3, EC_DIR_INPUT , 1, slave_1_pdos + 1, EC_WD_DISABLE},
+	{0xFF}
+	};
+	
+	/***************************************************/
+	
+	if (ecrt_slave_config_pdos(drive0, EC_END, slave_0_syncs))
+	{
+		printf("Failed to configure slave 0 PDOs\n");
+		return -1;
+	}
+	
+	if (ecrt_slave_config_pdos(drive1, EC_END, slave_1_syncs))
+	{
+		printf("Failed to configure slave 1 PDOs\n");
+		return -1;
+	}
 
+	unsigned int offset_controlWord0, offset_targetPos0, offset_statusWord0, offset_actPos0;
+	unsigned int offset_controlWord1, offset_targetPos1, offset_statusWord1, offset_actPos1;
+	
+	ec_pdo_entry_reg_t domain1_regs[] =
+	{
+	{0, 0, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord0},
+	{0, 0, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos0  },
+	{0, 0, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord0 },
+	{0, 0, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos0     },
+	
+	{0, 1, 0x00007595, 0x00000000, 0x6040, 0x00, &offset_controlWord1},
+	{0, 1, 0x00007595, 0x00000000, 0x607a, 0x00, &offset_targetPos1  },
+	{0, 1, 0x00007595, 0x00000000, 0x6041, 0x00, &offset_statusWord1 },
+	{0, 1, 0x00007595, 0x00000000, 0x6064, 0x00, &offset_actPos1     },
+	{}
+	};
+	
+	/* Creates a new process data domain. */
+	/* For process data exchange, at least one process data domain is needed. */
+	ec_domain_t* domain1 = ecrt_master_create_domain(master);
+	
+	/* Registers PDOs for a domain. */
+	/* Returns 0 on success. */
+	if (ecrt_domain_reg_pdo_entry_list(domain1, domain1_regs))
+	{
+		printf("PDO entry registration failed\n");
+		return -1;
+	}
+	
+	/* Up to this point, we have only requested the master. See log messages */
+	printf("Activating master...\n");
+	/* Important points from ecrt.h 
+	   - This function tells the master that the configuration phase is finished and
+	     the real-time operation will begin. 
+	   - It tells the master state machine that the bus configuration is now to be applied.
+	   - By calling the ecrt master activate() method, all slaves are configured according to
+             the prior method calls and are brought into OP state.
+	   - After this function has been called, the real-time application is in charge of cylically
+	     calling ecrt_master_send() and ecrt_master_receive(). Before calling this function, the 
+	     master thread is responsible for that. 
+	   - This method allocated memory and should not be called in real-time context.
+	*/
+	     
+	if (ecrt_master_activate(master))
+		return -1;
+
+	
+	uint8_t* domain1_pd;
+	/* Returns a pointer to (I think) the first byte of PDO data of the domain */
+	if (!(domain1_pd = ecrt_domain_data(domain1)))
+		return -1;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
