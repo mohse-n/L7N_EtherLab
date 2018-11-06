88c0386f84fb26aaaadc25b980cefcb388f179ba
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-11-06 15:51:25 +0330

    Output useful information

--- user/main.c
+++ user/main.c
@@ -579,6 +579,7 @@ int main(int argc, char **argv)
 		/* wakeupTime is also start time of the loop. */
 		/* execTime = endTime - wakeupTime */
 		timespec_sub(&execTime, &endTime, &wakeupTime);
+		printf("Execution time: %lu ns\n", execTime.tv_nsec);
 		#endif
 		
 		/* wakeupTime = wakeupTime + sleepTime */
@@ -600,7 +601,7 @@ int main(int argc, char **argv)
 		
 		#ifdef MEASURE_PERF
 		ecrt_master_reference_clock_time(master, &t_cur);
-		printf("%" PRIu32 "\n", t_cur - t_prev);
+		printf("Timestamp diff: %" PRIu32 " ns\n\n", t_cur - t_prev);
 		t_prev = t_cur;
 		#endif
 		
