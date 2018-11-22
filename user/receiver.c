4afdb757ed10f0a5a4485caa659b9bc05654b34d
Mohsen <mohsen.alizadeh@mail.um.ac.ir>
2018-11-22 11:54:51 +0330

    Update receiver.c

--- user/receiver.c
+++ user/receiver.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+/* #define LOG depends on #define MEASURE_PERF in main.c, so the latter should be defined in main.c if LOG is defined here. */
 #define LOG
 /* Assuming an update rate of exactly 1 ms, number of cycles for 24h = 24*3600*1000. */
 #define NUMBER_OF_CYCLES 86400000
